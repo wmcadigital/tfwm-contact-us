c503a2a292dce8918df6eda2e88899cb727aa029
Nathan Rogan <rolorogan@gmail.com>
2025-05-31 18:47:08 +0100

    add maintenance message (#159)
    Co-authored-by: Nathan Rogan <nathan.rogan@wmca.org.uk>

--- src/App/App.js
+++ src/App/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { useEffect, useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // Contexts
@@ -6,7 +7,23 @@ import { FormProvider, FormDataContext } from '../globalState/ContactUsContext';
 import EnquiryForm from './EnquiryForm/components';
 import './map.css';
 
+// Maintenance page component
+const MaintenancePage = () => (
+  <div style={{ textAlign: 'center', marginTop: '10%' }}>
+    <h1>We'll be back soon!</h1>
+    <p>Sorry for the inconvenience. We're performing some maintenance at the moment.</p>
+  </div>
+);
+
 function App() {
+  // Uncomment the next line to enable maintenance mode
+  const maintenanceMode = true; // Set to true to show maintenance page
+
+  if (maintenanceMode) {
+    return <MaintenancePage />;
+  }
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [{ formId }] = useContext(FormDataContext);
   const params = window.location.hash.slice(2);
   return (
