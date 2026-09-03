26aff79b3a4635958ed94813088ec726498936de
GeorgeEverett <George.Everett@wmca.org.uk>
2026-09-03 16:54:28 +0100

    Use 10 day response time for cycle-storage form responses

--- src/App/EnquiryForm/components/index.js
+++ src/App/EnquiryForm/components/index.js
@@ -17,8 +17,12 @@ const Complaint = () => {
   const formToLoad = formId || params;
   const { content } = Data.pages.find((data) => data.currentStepId === formToLoad);
   const day1 = content.warningText && content.warningText.includes('5') ? 5 : 2;
-  const days = content.warningText && content.warningText.includes('10') ? 10 : day1;
-  // const days = 10;
+  const days =
+    formToLoad === 'step-cycle-storage'
+      ? 10
+      : content.warningText && content.warningText.includes('10')
+      ? 10
+      : day1;
   useEffect(() => {
     const headerTitleEl = document.getElementById('formClicked');
     if (formToLoad === 'step-update-DD') {
